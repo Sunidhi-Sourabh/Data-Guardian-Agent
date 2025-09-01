ffee67b9eed74141f415e7a12ef792f060c9c799
Sunidhi Sourabh <sunidhi.sourabh@gmail.com>
2025-09-02 02:42:27 +0530

    charts.js

--- static/charts.js
+++ static/charts.js
@@ -1,1 +1,28 @@
+const riskData = JSON.parse(document.getElementById("risk-data").textContent);
+const patternData = JSON.parse(document.getElementById("pattern-data").textContent);
 
+new Chart(document.getElementById('riskChart'), {
+    type: 'pie',
+    data: {
+        labels: Object.keys(riskData),
+        datasets: [{
+            data: Object.values(riskData),
+            backgroundColor: ['#dc3545', '#fd7e14', '#198754', '#6c757d']
+        }]
+    }
+});
+
+new Chart(document.getElementById('patternChart'), {
+    type: 'bar',
+    data: {
+        labels: Object.keys(patternData),
+        datasets: [{
+            label: 'Matches',
+            data: Object.values(patternData),
+            backgroundColor: '#0d6efd'
+        }]
+    },
+    options: {
+        indexAxis: 'y'
+    }
+});
